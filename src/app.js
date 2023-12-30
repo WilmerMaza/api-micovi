c91d3f6c42bc4425b91ea23d0142fb1f4dd4bf6b
WilmerMaza <wilmermaza10@gmail.com>
2023-12-30 05:18:32 -0500

    Banner API

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const morgan = require('morgan');
 const routes = require('./routes.js');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config({path: '../../.env'});
+const path = require('path'); // Importa el módulo 'path' de Node.js
+require('dotenv').config({ path: '../../.env' });
 const { conn } = require('./db.js');
 
 require('./db.js');
@@ -18,8 +19,10 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json());
 server.use(cookieParser());
 server.use(morgan('dev'));
+
+server.use(express.static(path.join(__dirname, 'public'))); 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -29,19 +32,17 @@ server.use((req, res, next) => {
 server.use('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  
+server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
 });
 
-const {env:{PORT}} = process
+const { env: { PORT } } = process;
 server.listen(PORT || 3002, () => {
-  console.log(`Server listening on port ${PORT??3002}!`);
+  console.log(`Server listening on port ${PORT ?? 3002}!`);
   conn.sync({ force: false });
-
 });
 
 module.exports = server;
